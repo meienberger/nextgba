231f5a872331b589bee27427e2d36d41ea4f5085
Nicolas Meienberger <github@thisprops.com>
2024-08-31 12:07:46 +0100

    refactor: setup remix app

---
+++ remix/app/root.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Hello world!</h1>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
